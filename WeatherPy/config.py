102db047bb23ecb6e63370141ed942e728de6217
arvinseeva <65349222+arvinseeva@users.noreply.github.com>
2020-07-19 01:39:27 +0800

    Removed API keys

--- WeatherPy/config.py
+++
@@ -1,3 +0,0 @@
-#  Add your API key
-api_key = "ee467695e172a328aaf6c7408f73914d"
-g_key = "AIzaSyBqN2psucDcj3dg_OaiSX1YZjpQhtTuFV4"
--- WeatherPy/gkey.py
+++
@@ -1,2 +0,0 @@
-#  Add your API key
-api_key = "AIzaSyBqN2psucDcj3dg_OaiSX1YZjpQhtTuFV4"
